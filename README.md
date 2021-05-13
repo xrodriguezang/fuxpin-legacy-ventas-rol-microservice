07b2ba43de9af9a3da54bbce6b14d92d36bf3895
xrodrig <xrodrig@dipta.cat>
2021-05-13 19:44:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ https://spring.io/blog/2015/07/14/microservices-with-spring
 https://www.baeldung.com/spring-cloud-netflix-eureka
 
 https://stackoverflow.com/questions/63026751/json-decoding-error-root-name-timestamp-does-not-match-expected-eureka-sprin
+
+https://spring.io/guides/gs/service-registration-and-discovery/
+
+https://howtodoinjava.com/spring-cloud/spring-cloud-service-discovery-netflix-eureka/
--- src/main/java/unir/tfg/fuxpin/fuxpinlegacyventasrolmicroservice/FuxpinLegacyVentasRolMicroserviceApplication.java
+++ src/main/java/unir/tfg/fuxpin/fuxpinlegacyventasrolmicroservice/FuxpinLegacyVentasRolMicroserviceApplication.java
@@ -1,19 +1,19 @@
 package unir.tfg.fuxpin.fuxpinlegacyventasrolmicroservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import unir.tfg.fuxpin.fuxpinlegacyventasrolmicroservice.model.RoleLegacy;
 
+import java.util.List;
+
 @SpringBootApplication
-@EnableEurekaClient
 public class FuxpinLegacyVentasRolMicroserviceApplication {
 
 	public static void main(String[] args) {
@@ -29,6 +29,15 @@ public class FuxpinLegacyVentasRolMicroserviceApplication {
 
 @RestController
 class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 
 	@GetMapping("/getroles/{id}")
 	public ResponseEntity<RoleLegacy> read(@PathVariable("id") Long id) {
